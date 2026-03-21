772ec7a4530336158492dd2e3c272b20358e7d2d
jtranLV12 <jvtran1@my.loyno.edu>
2026-03-21 12:39:19 -0500

    Update

--- PlantGrowth.js
+++ PlantGrowth.js
@@ -1,4 +1,19 @@
 import goalData from '.\PlantGoals.json' assert {type: 'json'}
+let plantProgress = 0;
 
+function DisplayGoals() {
 console.log(goalData);
 console.log(goalData[0].Goal);
+}
+
+function GrowPlant() {
+    let playerResponse = true;
+    if (playerResponse == true) {
+        plantProgress +=1;
+    }
+    console.log("Current Growth Amount: " + plantProgress)
+}
+
+function GrowthStage () {
+    if ()
+}
