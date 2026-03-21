ceaed1513ca825353aa1bf949e80ca888b4d2c16
jtranLV12 <jvtran1@my.loyno.edu>
2026-03-21 14:07:31 -0500

    Update

--- Classes.js
+++ Classes.js
@@ -1,8 +1,8 @@
 import goalData from './PlantGoals.json' assert {type: 'json'}
 class Plant {
-constructor(goal) {
-        this.goalId = goalData.id;
-        this.goalName = goalData.goal;
+constructor(goal) {     //Plant class initializes with goal data from JSON file
+        this.goalId = goal.GoalID;
+        this.goalName = goal.Goal;
         this.progress = 0;
     }
 }
